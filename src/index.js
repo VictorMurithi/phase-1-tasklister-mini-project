f57d283cbcc48035a2edc023544a4da3b0838675
victor <victor.murithi@student.moringaschool.com>
2023-09-28 12:44:11 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const form = document.querySelector('form')
-    const input = document.querySelector('new-task-description')
-    const list = document.querySelector('tasks')
-
-    form.addEventListener("submit", function(e){
-        e.preventDefault();
-        const inputText = input.value;
-        const taskList = document.createElement('li')
-        taskList.textContent = inputText;
-        list.appendChild(taskList);
+    let form =document.querySelector('form')
+    form.addEventListener('submit', (e) =>{
+        e.preventDefault()
+        handleTasks(e.target['new-task-description'].value)
+        form.reset()
     })
 })
+ function handleTasks(task){
+    let li = document.createElement('li');
+    let btn = document.createElement("button")
+    btn.addEventListener('click',handleDelete)
+    btn.textContent = " x "
 
+    let taskText = document.createTextNode(task);
+
+    li.appendChild(taskText)
+    li.appendChild(btn)
+
+    document.querySelector('#tasks').appendChild(li)
+ }
+function handleDelete(e){
+    e.target.parentNode.remove()
+}
